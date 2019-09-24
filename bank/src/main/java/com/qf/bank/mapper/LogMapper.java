cec3ae9f5597d6a4fe24ca0e0ce8d53506f4fe45
sgy <631407812@qq.com>
2019-09-24 15:59:08 +0800

    注释

--- bank/src/main/java/com/qf/bank/mapper/LogMapper.java
+++ bank/src/main/java/com/qf/bank/mapper/LogMapper.java
@@ -6,7 +6,7 @@ import com.qf.bank.util.QueryVo;
 import java.util.List;
 
 public interface LogMapper {
-    Boolean insertLog(Log log);
+    //Boolean insertLog(Log log);
 
     List<Log> selectLog(QueryVo queryVo);
 
